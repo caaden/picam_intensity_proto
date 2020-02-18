95c809b86b9d3e11a495ac2c1999fad26bf08e27
Chris Aden <caden@fringeai.com>
2020-02-18 14:09:56 -0500

    new commit

--- capture.py
+++ capture.py
@@ -8,6 +8,14 @@ import cv2
 with picamera.PiCamera() as camera:
     camera.resolution = (640,480)
     camera.framerate = 24
+    camera.exposure_mode='off' 
+    camera.awb_mode='off'
+    camera.shutter_speed=500
+    camera.awb_gains=(fractions.Fraction(183, 128), fractions.Fraction(153, 128))  
+    camera.contrast=0
+    camera.brightness=50
+    camera.sharpness=0
+    camera.saturation=0
     time.sleep(2)
     while True:   
         output = np.empty((480, 640, 3), dtype=np.uint8) #swap x and y
